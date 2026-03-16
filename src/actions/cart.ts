ca585486b5f046e2c045f310068035b38fbcfc93
Debos Das <debos.das.02@gmail.com>
2026-03-17 05:48:33 +0600

    fixed cart count error

--- src/actions/cart.ts
+++ src/actions/cart.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { authOptions } from "@/helpers/authOptions";
 import axiosInstance from "@/lib/axiosInstance";
+import { getServerSession } from "next-auth";
 import { revalidateTag } from "next/cache";
 
 // ✅ CREATE CART
@@ -21,17 +23,27 @@ export const createCart = async (data: { productId: number; quantity: number; })
 
 
 // ✅ GET CART COUNT (FETCH ONLY)
+// src/actions/cart.ts
+
 export const getCartCount = async () => {
     try {
-        const res = await axiosInstance.get('/cart/count');
-        revalidateTag("cart-count");
-        console.log("res-from-api", res);
-        return res?.data?.data ?? 0;
+        const session = await getServerSession(authOptions);
+        const token = (session?.user as any)?.accessToken;
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/cart/count`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            next: { tags: ["cart-count"] }, // ✅ এখন revalidateTag কাজ করবে
+        });
+
+        if (!res.ok) return 0;
+        const data = await res.json();
+        return data?.data ?? 0;
     } catch {
         return 0;
     }
 };
-
 
 // ✅ GET CART LIST
 export const getCart = async () => {
